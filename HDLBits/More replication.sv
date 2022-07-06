bcf4baf256b4de6cd1715ec6404242cb9c5664d3
bolero021 <87541401+bolero021@users.noreply.github.com>
2022-07-06 11:53:13 +0800

    delete repeated files

--- HDLBits/More replication.sv
+++
@@ -1,7 +0,0 @@
-module top_module (
-    input  logic        a, b, c, d, e,
-    output logic [24:0] out );
-
-    assign out = ~{{5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}}} ^ {5{a, b, c, d, e}};
-
-endmodule
--- HDLBits/Three modules.sv
+++
@@ -1,8 +0,0 @@
-module top_module ( input logic clk, input logic d, output logic q );
-
-	logic w1, w2;
-
-	my_dff instance1 (.clk(clk), .d(d), .q(w1));
-	my_dff instance2 (.clk(clk), .d(w1), .q(w2));
-	my_dff instance3 (.clk(clk), .d(w2), .q(q));
-endmodule
